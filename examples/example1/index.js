ea43c6db477a8c02778d0b1daa35b599b573a33b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-05 17:36:20 +0100

    Create index.js

---
+++ examples/example1/index.js
@@ -0,0 +1,6 @@
+// examples/example1/index.js
+import { STACKS_API_MAINNET } from 'stx-portfolio-sdk/config.js';
+
+console.log("Integration Test Running...");
+console.log("Connecting to:", STACKS_API_MAINNET);
+console.log("✅ SDK Load Successful");
